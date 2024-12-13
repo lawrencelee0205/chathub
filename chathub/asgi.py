df587f6ec77d1e80f26a5344561a07d7d057e3a3
lawrencelee0205 <lawrencelee0205.dev@gmail.com>
2024-12-13 15:59:18 +0800

    CH-1 Setup django, docker compose and test.

---
+++ chathub/asgi.py
@@ -0,0 +1,22 @@
+"""
+ASGI config for chathub project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chathub.settings')
+
+# application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    # WebSocket protocol will be added later for real-time chat
+})
+
